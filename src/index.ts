61691793c0ae1143bdc0c02aea1c0a5e03a85e59
sprhawk <465558+sprhawk@users.noreply.github.com>
2019-11-04 23:18:16 +0800

    changed to let instead of var

--- src/index.ts
+++ src/index.ts
@@ -14,22 +14,22 @@ import "@babylonjs/core/Meshes/meshBuilder";
 const canvas = document.getElementById("mainCanvas") as HTMLCanvasElement;
 
 const engine = new Engine(canvas);
-var scene = new Scene(engine);
+let scene = new Scene(engine);
 
-var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+let camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 camera.setTarget(Vector3.Zero());
 camera.attachControl(canvas, true);
 
-var light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
+let light = new HemisphericLight("light1", new Vector3(0, 1, 0), scene);
 light.intensity = 0.7;
 
-var material = new GridMaterial("grid", scene);
+let material = new GridMaterial("grid", scene);
 
-var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
+let sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
 sphere.position.y = 2;
 sphere.material = material;
 
-var ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
+let ground = Mesh.CreateGround("ground1", 6, 6, 2, scene);
 ground.material = material;
 
 engine.runRenderLoop(() => {
